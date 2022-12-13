5185c7b88d1f980b2aa895742536f4679b0c8f7e
BrunaScauri <brunascauri@hotmail.com>
2022-12-13 20:11:12 -0300

    feat: image and header added

--- pigeoneat/src/App.jsx
+++ pigeoneat/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import pigeonWaits from './resources/pigeon_waits.webp';
 
 function App() {
   return (
     <div>
       <Header />
-      <div />
+      <div className="mainPhoto">
+        <img className="mainImg" src={pigeonWaits} alt="pigeon is waiting" width="250" height="250" />
+      </div>
     </div>
   );
 }
--- pigeoneat/src/components/Header.jsx
+++ pigeoneat/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Header.css';
 
 function Header() {
   return (
     <header>
-      <h1>
+      <h1 className="header">
         Pigeon Eat
       </h1>
     </header>
